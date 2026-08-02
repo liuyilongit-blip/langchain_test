edc2920ce744b2477f0cc92f546c6e532dfdb296
刘玉龙 <1037180803@qq.com>
2026-08-02 19:44:34 +0800

    初始化环境

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,18 @@
+import asyncio
 import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
 from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chromadb import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (Colors, log_error, log_header, log_info, log_success, log_warning)
 
 load_dotenv()
 
@@ -8,10 +21,27 @@ os.environ['HTTPS_PROXY'] = ''
 os.environ['http_proxy'] = ''
 os.environ['https_proxy'] = ''
 
+# Configure SSL context to use certifi certificates
+# ssl_context = ssl.create_default_context(cafile=certifi.where())
+# os.environ["SSL_CERT_FILE"] = certifi.where()
+# os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OpenAIEmbeddings(
+    model="BAAI/bge-m3",
+    show_progress_bar=False, # 显示进度条
+    chunk_size=50, # 每个批次的最大文档数
+    retry_min_seconds=10, # 重试API调用前的最短等待秒数
+)
+# chroma=Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+vectorstore = PineconeVectorStore(index_name="langchain-doc-index",embedding=embeddings)
+tavily_extract = TavilyExtract()
+tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
 from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
+
+
+
 print('Ingesting...')
 loader = TextLoader("./text.txt", encoding="utf-8")
 document = loader.load()
@@ -19,7 +49,7 @@ print('splitter...')
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, chunk_overlap=0)
 texts = text_splitter.split_documents(document)
 print('ingesting...')
-embeddings = OpenAIEmbeddings(model="BAAI/bge-m3")
+
 PineconeVectorStore.from_documents(texts, embeddings,index_name=os.environ['INDEX_NAME'])
 print('finish')
 
